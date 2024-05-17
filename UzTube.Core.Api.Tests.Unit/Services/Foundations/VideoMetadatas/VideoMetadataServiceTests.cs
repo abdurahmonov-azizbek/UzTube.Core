4d02ee6ca972207ce9ad15fa0e3c36eb97675abc
Azizbek Abdurahmonov <abdurahmonov-azizbek@outlook.com>
2024-05-17 03:42:34 -0700

    ShouldAddVideoMetadataAsync -> FAIL

---
+++ UzTube.Core.Api.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.cs
@@ -0,0 +1,43 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using Moq;
+using Tynamix.ObjectFiller;
+using UzTube.Core.Api.Brokers.Storages;
+using UzTube.Core.Api.Models.VideoMetadatas;
+using UzTube.Core.Api.Services.VideoMetadatas;
+
+namespace UzTube.Core.Api.Tests.Unit.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IVideoMetadataService videoMetadataService;
+
+        public VideoMetadataServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.videoMetadataService = new VideoMetadataService(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        public static VideoMetadata CreateRandomVideoMetadata() =>
+            CreateVideoMetadataFiller().Create();
+
+        private static Filler<VideoMetadata> CreateVideoMetadataFiller()
+        {
+            var filler = new Filler<VideoMetadata>();
+
+            filler.Setup()
+                    .OnType<DateTimeOffset>().Use(GetRandomDateTime);
+
+            return filler;
+        }
+    }
+}
---
+++ UzTube.Core.Api/Services/VideoMetadatas/IVideoMetadataService.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using UzTube.Core.Api.Models.VideoMetadatas;
+
+namespace UzTube.Core.Api.Services.VideoMetadatas
+{
+    internal interface IVideoMetadataService
+    {
+        ValueTask<VideoMetadata> AddVideoMetadataAsync(VideoMetadata videoMetadata);
+    }
+}
