a0a4e91b133048e2e6fa2ebcd5533ae161957c20
Azizbek Abdurahmonov <abdurahmonov-azizbek@outlook.com>
2024-05-29 06:31:20 -0700

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> Fail

---
+++ UzTube.Core.Api.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,57 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using UzTube.Core.Api.Models.VideoMetadatas.Exceptions;
+
+namespace UzTube.Core.Api.Tests.Unit.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            //given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageException =
+                new FailedVideoMetadataStorageException(
+                    message: "Failed video metadata error occured, contact support.",
+                    innerException: sqlException);
+
+            var expectedVideoMetadataDependencyException =
+                new VideoMetadataDependencyException(
+                    message: "Video metadata dependency error occured, fix the errors and try again.",
+                    innerException: failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllVideoMetadatas()).Throws(sqlException);
+
+            //when
+            Action retrieveAllVideoMetadatasAction = () =>
+                this.videoMetadataService.RetrieveAllVideoMetadatas();
+
+            VideoMetadataDependencyException actualVideoMetadataDependencyException =
+                Assert.Throws<VideoMetadataDependencyException>(retrieveAllVideoMetadatasAction);
+
+            //then
+            actualVideoMetadataDependencyException.Should()
+                .BeEquivalentTo(expectedVideoMetadataDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllVideoMetadatas(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedVideoMetadataDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
